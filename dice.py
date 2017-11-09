2b4f826eb93401101e3b5da67f11926971eaef54
poudus <poudus@protonmail.ch>
2017-11-09 21:53:38 +0100

    new function search_run_counter

--- dice.py
+++ dice.py
@@ -1,26 +1,17 @@
 import random
 import sys
 
-# fonction inutile, en ligne 58 je teste directement if counter.count(nb) > 0
-def search_same(text, nb):
-	count = counter.count(nb)
-	if count > 0:
-		print(str(count)+' '+text)
-		return True
-	else:
-		return False
-
 # search run from start position
 def search_run_from(start, length):
 	for i in range(length):
-		if sortedDices[start+i] != sortedDices[start]+i:
+		if counter[start+i] == 0:
 			return False
 	return True
 
 # search run from all valid start positions
-def search_run(length, nb):
+def search_run_counter(length):
 	nb_run = 0
-	for i in range(nb-length+1):
+	for i in range(1, 6-length+2):
 		if search_run_from(i, length):
 			nb_run += 1
 	return nb_run
@@ -46,14 +37,10 @@ for dice in dices:
 
 print(counter)
 
-nb_run2 = search_run(2, nb_dices)
-if (nb_run2 > 0):
-	print(str(nb_run2)+' suites de longueur 2')
+nb_run4 = search_run_counter(4)
+if (nb_run4 > 0):
+	print(str(nb_run4)+' suites de longueur 4')
 
-nb_run3 = search_run(3, nb_dices)
+nb_run3 = search_run_counter(3)
 if (nb_run3 > 0):
 	print(str(nb_run3)+' suites de longueur 3')
-
-# example pour tester une paire et une serie de 3
-if counter.count(2) > 0 and search_run(3, nb_dices) > 0:
-	print('paire et serie de 3')
